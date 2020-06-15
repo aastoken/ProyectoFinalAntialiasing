c7f9defea8970d61f8d52a54d070ce6efb9d2e3c
El cremas <eduexile@gmail.com>
2020-06-15 19:34:08 +0200

    Init variables

--- Demo4/Demo4/Shaders/PostEffectPS.hlsl
+++ Demo4/Demo4/Shaders/PostEffectPS.hlsl
@@ -13,6 +13,24 @@ SamplerState samplerState : register( s0 );
 
 float3 main( OutputVS input ) : SV_TARGET
 {
+	FxaaFloat2 pos = float2(0.0,0.0);
+	FxaaFloat4 fxaaConsolePosPos = float4(0,0,0,0);
+	FxaaTex tex = { samplerState, sceneTexture };
+	FxaaTex fxaaConsole360TexExpBiasNegOne = { samplerState, sceneTexture };
+	FxaaTex fxaaConsole360TexExpBiasNegTwo = { samplerState, sceneTexture };
+	FxaaFloat2 fxaaQualityRcpFrame = float2(1.0 / (1280.0), 1.0 / (720.0));
+	FxaaFloat4 fxaaConsoleRcpFrameOpt = float4(0, 0, 0, 0);
+	FxaaFloat4 fxaaConsoleRcpFrameOpt2 = float4(0, 0, 0, 0);
+	FxaaFloat4 fxaaConsole360RcpFrameOpt2 = float4(0, 0, 0, 0);
+	FxaaFloat fxaaQualitySubpix = float(1.0);
+	FxaaFloat fxaaQualityEdgeThreshold = float(0.063);
+	FxaaFloat fxaaQualityEdgeThresholdMin = float(0.0312);
+	FxaaFloat fxaaConsoleEdgeSharpness = float(4.0);
+	FxaaFloat fxaaConsoleEdgeThreshold = float(0.125);
+	FxaaFloat fxaaConsoleEdgeThresholdMin = float(0.05);
+	FxaaFloat4 fxaaConsole360ConstDir = float4(1.0, -1.0, 0.25, -0.25);
+
+
 	//return float3( 0.0, 0.0, 0.0 ); wrong
 
 	return sceneTexture.Sample(samplerState , input.texcoord).rgb;
